d2fe4498fb52b9f5621788856a0023554da24664
yjxqwed <yjxqwed@umich.edu>
2020-04-10 22:40:36 -0400

    Initial Commit

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+    private SceneController sc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sc = GameObject.Find("SceneController").GetComponent<SceneController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    //public GameObject sceneController;
+
+    public void LoadScene(int index)
+    {
+        sc.LoadScene(index);
+    }
+}
