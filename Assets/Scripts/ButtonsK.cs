0313e926f7c8ccf21acbf917e9f4462544c72d20
Jiaxing Yang <yjxqwed@umich.edu>
2020-04-11 14:12:03 -0400

    Android Demo

---
+++ Assets/Scripts/ButtonsK.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonsK : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public GameObject textsPanel;
+    public GameObject visualsPanel;
+    public GameObject buttonsPanel;
+
+    public void SetTexts(bool enable)
+    {
+        buttonsPanel.SetActive(!enable);
+        textsPanel.SetActive(enable);
+    }
+
+    public void SetVisuals(bool enable)
+    {
+        buttonsPanel.SetActive(!enable);
+        visualsPanel.SetActive(enable);
+    }
+
+    public void Intro()
+    {
+        Application.OpenURL("https://en.wikipedia.org/wiki/Tadeusz_Ko%C5%9Bciuszko");
+    }
+}
---
+++ Assets/Scripts/VisualsOnEnable.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisualsOnEnable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        vp.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public UnityEngine.Video.VideoPlayer vp;
+
+    private void OnEnable()
+    {
+        vp.Play();
+    }
+
+    private void OnDisable()
+    {
+        vp.Stop();
+    }
+}
